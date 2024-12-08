8d9e42bc159f17b283165e0b1e1bd9e7b29e2b3e
Chris <toilatuancuong@gmail.com>
2024-12-08 16:24:50 +0700

    Errors

---
+++ 5_ERORS/001-intro.py
@@ -0,0 +1,13 @@
+# Traceback : Where the error happend
+# Stacktraces: 
+#1. At every bottom it gives you the error that was raised and a desscription
+#2. What line of your code raised the error
+#3. What function that line is in
+#4 What function called the function that the line is in
+
+# How to deal with Eror?
+1. Look at your code
+2. Put the error and message into GG, see if something comes up with Stackoverflow
+3. Look at the code , this time more slowly, and run through it as if you were a computer. Notice if anything that could potentially be a source of the error
+4. Run only someparts of the code in isolation, that'll help identify which part of your code is giving you an error
+5. Use a debugger
---
+++ 5_ERORS/002-build-in-errors-in-Python.py
@@ -0,0 +1,41 @@
+# These types of error happened when you access that didn't exist in the wrong way
+#IndexError
+#KeyError
+#NameError
+#AttributError -> Fairly related 
+#NotImplementedError
+#RuntimeError
+#SytaxError
+#IndextationError
+#TabError  -> spaces or tab
+#TypeError  -> Give wrong type
+#ValueError
+#ImportError
+#DeprecationWarning 
+
+#1.IndexError -> Access the index did not exist or incorrect
+#2. Keyerror -> 
+#3. NameError -> Variable is not defined
+#4. AttributError: Dealing with object -> Object doens't have this attribute
+#5. NotImplementedError -> Point that we haven't do with this
+#5. RunTimeError: A base error, base class, Others extend this one to be Runtime Errors
+#6. Syntaxerror: When Mess up with python
+class User # leak ':' -> SyntaxError
+	def __init__(self,username,password):
+		self.username=username
+		self.password=password
+#7. IndextationError
+def add_two(x,y):
+	pass
+
+#8. TabError -> spaces or tab
+
+#9.TypeError
+print(5+'hi') # TypeError: int + str
+
+#10. ValueError: 
+int('20.5') #-> number not stringte
+# note : Normally only built in function will raise a Value Error 
+#11. ImportError: 
+#12. DeprecationWarning : Something wrong happened -> Program will crash -> Deprecate: No longer bestway of doing something
+#
